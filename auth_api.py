511912f2543729e8d32b488417e2de7fc77f722e
fmelan <fmelan@gmail.com>
2022-04-01 09:08:48 +0200

    Auth API calls. Refactoring.

--- auth_api.py
+++ auth_api.py
@@ -9,14 +9,7 @@ test_surveillance_base_uri = "https://surveillance-api.sit.altitudeangel.io"
 test_auth_base_uri = "https://auth.sit.altitudeangel.io"
 
 
-class DeviceTokensResponse(BaseModel):
-    access_token: str
-    token_type: str
-    expires_in: int
-    refresh_token: str
-
-
-class DeviceTokensRequest(BaseModel):
+class AccessTokensRequest(BaseModel):
     client_id: str
     client_secret: str
     grant_type: str
@@ -27,10 +20,25 @@ class DeviceTokensRequest(BaseModel):
     state: Optional[str]
 
 
-def get_device_token(request_data):
-    """
-    https://docs.altitudeangel.com/docs/altitude-angel-identity-provider
+class AccessTokensResponse(BaseModel):
+    access_token: str
+    token_type: str
+    expires_in: int
+    refresh_token: str
+    refresh_token: str
+    state: Optional[str]
 
+
+class RefreshAccessTokenRequest(BaseModel):
+    client_id: str
+    client_secret: str
+    grant_type: str
+    refresh_token: str
+    state: Optional[str]
+
+
+def get_access_token(client_id, client_secret, redirect_uri, device_id, state=None):
+    """
     Before this call, users must have pre-registered their local unique sensor IDs by creating a support ticket.
     Function will call API to obtain standard OAuth v2 response with access_token and refresh_token. Both these
     tokens will be returned by the function. The access_token should be then used to communicate with the
@@ -41,18 +49,67 @@ def get_device_token(request_data):
 
     # TODO common request calls in one method
 
+    request_data = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': "client_credentials",
+        'scope': "surveillance_api",
+        'token_format': 'jwt',
+        'redirect_uri': redirect_uri,
+        'device_id': device_id
+    }
+
+    if state:
+        request_data['state'] = state
+
     try:
-        device_token_req = DeviceTokensRequest(**request_data)
+        access_token_req = AccessTokensRequest(**request_data)
     except ValidationError as e:
         raise AngelError(e.json())
 
-    r = httpx.post(f"{test_auth_base_uri}/oauth/v2/token", headers=headers, data=device_token_req.dict())
+    resp = httpx.post(f"{test_auth_base_uri}/oauth/v2/token", headers=headers, data=access_token_req.dict())
 
-    if r.status_code != 200:
-        raise AngelError(r.text)
+    if resp.status_code != 200:
+        err = AngelError(resp.text)
+        err.status_code = resp.status_code
+        raise err
 
-    return DeviceTokensResponse(**r.json())
+    return AccessTokensResponse(**resp.json())
 
 
-def refresh_device_token():
-    pass
+def refresh_access_token(client_id, client_secret, refresh_token, state=None):
+    """
+    For every request made to the Altitude Angel APIs with a bearer access token, you must check the response code for
+    an 401 Access Denied error.
+
+    When this is received and using a previously valid access token, you should make a request for a new access token.
+
+    Once a new access token is received then retry the failed request using the new access token. If this also results
+    in the same response code then an error should be returned to the user.
+    :return:
+    """
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+
+    request_data = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': "refresh_token",
+        'refresh_token': refresh_token,
+    }
+
+    if state:
+        request_data['state'] = state
+
+    try:
+        refresh_access_token_req = RefreshAccessTokenRequest(**request_data)
+    except ValidationError as e:
+        raise AngelError(e.json())
+
+    resp = httpx.post(f"{test_auth_base_uri}/oauth/v2/token", headers=headers, data=refresh_access_token_req.dict())
+
+    if resp.status_code != 200:
+        err = AngelError(resp.text)
+        err.status_code = resp.status_code
+        raise err
+
+    return AccessTokensResponse(**resp.json())
--- main.py
+++ main.py
@@ -1,33 +1,34 @@
-from auth_api import get_device_token
+from auth_api import get_access_token
 
 
 class AngelError(Exception):
-    pass
+    status_code: int
 
 
 def get_test_tokens():
     data = {
         'client_id': 'RziveGzb0G4z6M-1cFQkE6MRhTVtWX2U8pna0klD0',
-        'client_secret': "vXBk/5yO1tx9eH9BLMNpJn403CJariLJ3eL8umy8Ucl1EZn/j6GkoA==",
-        'grant_type': "client_credentials",
-        'scope': "surveillance_api",
-        'token_format': 'jwt',
+        'client_secret': 'RziveGzb0G4z6M-1cFQkE6MRhTVtWX2U8pna0klD0',
         'redirect_uri': "localhost",
         'device_id': "DroneTag"
     }
 
-    return get_device_token(data)
-
+    return get_access_token(**data)
 
 if __name__ == '__main__':
-
-
     # The access and refresh tokens should ideally be stored on the client to prevent the user having to authenticate
     # repeatedly. How to store these is beyond the scope of this tutorial, but a client-side cookie, local storage
     # etc could be used.
 
-    tokens = get_test_tokens()
+    try:
+        tokens = get_test_tokens()
+    except AngelError as e:
+        print(e.status_code)
+        print(e.json())
 
     print(tokens.dict())
     print(tokens.access_token)
+
+
+
 
